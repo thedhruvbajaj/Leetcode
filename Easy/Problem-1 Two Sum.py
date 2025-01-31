38da81536ad74900cb05f8bcbf8c9a5467837b50
Dhruv Bajaj <thedhruvbajaj@gmail.com>
2025-02-01 00:50:58 +0530

    Update Problem-1 Two Sum.py

--- Easy/Problem-1 Two Sum.py
+++ Easy/Problem-1 Two Sum.py
@@ -1,17 +1,13 @@
+from typing import List
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # Dictionary to store previously seen numbers and their indices
-        prevMap = {}  
+        prevMap = {}  # Dictionary to store previously seen numbers and their indices  
 
-        # Loop through the array
-        for i, n in enumerate(nums):  
-            # Compute the required difference
-            diff = target - n  
+        for i, n in enumerate(nums):  # Loop through the array  
+            diff = target - n  # Compute the required difference  
 
-            # Check if the difference exists in the hashmap
-            if diff in prevMap:  
-                # Return indices of the two numbers
-                return [prevMap[diff], i]  
+            if diff in prevMap:  # Check if the difference exists in the hashmap  
+                return [prevMap[diff], i]  # Return indices of the two numbers  
 
-            # Store the current number and its index in the hashmap
-            prevMap[n] = i  
+            prevMap[n] = i  # Store the current number and its index in the hashmap  
